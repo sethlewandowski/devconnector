457cfb3ee330f029043a0fa08682489ac21543c4
sethl <lewandowski.seth@gmail.com>
2020-08-26 16:09:25 -0700

    saved work from the day

---
+++ middleware/auth.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const request = require("request");
+
+module.exports = function (req, res, next) {
+  // get token from header
+  const token = req.header("x-auth-token");
+
+  //check if no token
+  if (!token) {
+    return res.status(401).json({ msg: "no token, Authorization denied" });
+  }
+
+  // verify token
+  try {
+    const decoded = jwt.verify(token, config.get("jwtSecret"));
+
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: "Token is not valid" });
+  }
+};
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
 
 // @route         GET api/auth
 // @description   Test route
 // @access        Public
-router.get("/", (req, res) => res.send("Auth route"));
+// by adding 'auth' as second param in get method we run the request through the auth middleware
+router.get("/", auth, (req, res) => res.send("Auth route"));
 
 module.exports = router;

8bbdef054f52c35dd1ddf5199a287dce2f286d30
sethl <lewandowski.seth@gmail.com>
2020-09-01 16:26:33 -0700

    added response to authorized user get request to return user data less password

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,10 +2,19 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth");
 
+const User = require("../../models/User");
+
 // @route         GET api/auth
 // @description   Test route
 // @access        Public
 // by adding 'auth' as second param in get method we run the request through the auth middleware
-router.get("/", auth, (req, res) => res.send("Auth route"));
-
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password"); // find the user
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
 module.exports = router;

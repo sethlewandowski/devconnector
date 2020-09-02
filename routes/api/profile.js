d4eecf9118e45295f73d83eb803c4be5f69559dd
sethl <lewandowski.seth@gmail.com>
2020-09-02 16:54:22 -0700

    added delete user route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -143,4 +143,24 @@ router.get("/user/:user_id", async (req, res) => {
   }
 });
 
+// @route         DELETE api/profile
+// @description   Delete profile, user, & posts
+// @access        Private
+router.delete("/", auth, async (req, res) => {
+  try {
+    // @ todo - remove users posts
+
+    // Remove profile
+    await Profile.findByIdAndRemove({ user: req.user.id });
+
+    // Remove User
+    await User.findOneAndRemove({ _id: req.user.id });
+
+    res.json({ msg: "User Deleted" });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;

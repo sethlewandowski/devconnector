9e425613b398fe6126137544a517daf8f27f6845
sethl <lewandowski.seth@gmail.com>
2020-09-07 17:30:05 -0700

    added the route for getting a specific post by id

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -52,4 +52,24 @@ router.get("/", auth, async (req, res) => {
   }
 });
 
+// @route         GET api/posts/:id
+// @description   Get post by ID
+// @access        Private
+router.get("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id); //req.params.id lets us get id from request
+
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;

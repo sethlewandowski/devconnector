0d339a136683d2387c42a35149126227e085b07c
sethl <lewandowski.seth@gmail.com>
2020-09-07 18:00:05 -0700

    added like a post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -100,4 +100,30 @@ router.delete("/:id", auth, async (req, res) => {
   }
 });
 
+// @route         PUT api/posts/like/:id
+// @description   Like a post
+// @access        Private
+router.put("/like/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    //check if post has already been liked by user (no double liking!)
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id).length >
+      0
+    ) {
+      return res.json(400).json({ msg: "Post already liked" });
+    }
+
+    post.likes.unshift({ user: req.user.id });
+
+    await post.save(); //save the altered post
+
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;

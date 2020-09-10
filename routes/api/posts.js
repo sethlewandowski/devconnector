93cd9b6e1cd186b7a773ffa06ba423ccce0cdcf9
sethl <lewandowski.seth@gmail.com>
2020-09-10 13:30:48 -0700

    added add comment route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -157,4 +157,39 @@ router.put("/unlike/:id", auth, async (req, res) => {
   }
 });
 
+// @route         POST api/post/comment/:id
+// @description   Comment on a post
+// @access        Private
+router.post(
+  "/comment/:id",
+  [auth, [check("text", "Text is required").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+      const post = await Post.findById(req.params.id); // gets us the post
+
+      const newComment = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      };
+
+      post.comments.unshift(newComment);
+
+      await post.save();
+
+      res.json(post.comments); //once we get the post, send it back in the response
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
 module.exports = router;

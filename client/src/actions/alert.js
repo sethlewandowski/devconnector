b94472208721202ecce0b117477818315684ec30
sethl <lewandowski.seth@gmail.com>
2020-09-11 16:41:39 -0700

    saving work

---
+++ client/src/actions/alert.js
@@ -0,0 +1,15 @@
+import uuid from 'uuid';
+import { SET_ALERT, REMOVE_ALERT } from './types';
+
+export const setAlert = (msg, alertType) => (dispatch) => {
+  //enabled by thunk middleware
+  const id = uuid.v4();
+  dispatch({
+    type: SET_ALERT,
+    payload: {
+      msg,
+      alertType,
+      id,
+    },
+  });
+};

5a837c621960504e05bf95d9732d5142ab87b601
sethl <lewandowski.seth@gmail.com>
2020-09-11 16:28:40 -0700

    configured alert.js

--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -1,1 +1,16 @@
+import { SET_ALERT, REMOVE_ALERT } from '../actions/types';
+
 const initalState = [];
+
+export default function (state = initalState, action) {
+  const { type, payload } = action; // pull out type and payload to make easier to read
+
+  switch (type) {
+    case SET_ALERT:
+      return [...state, payload];
+    case REMOVE_ALERT:
+      return state.filter((alert) => alert.id !== payload);
+    default:
+      return state;
+  }
+}

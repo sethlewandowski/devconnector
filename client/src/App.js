2d73bd02348882a721b5dbe1d902fb8b17f5b15a
sethl <lewandowski.seth@gmail.com>
2020-09-10 17:02:40 -0700

    lots of stuff, mostly configuring pages for Login and Register

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
-import React, { Fragment } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Navbar from "./components/layout/Navbar";
-import Landing from "./components/layout/Landing";
-import Register from "./components/auth/Register";
-import Login from "./components/auth/Login";
-import "./App.css";
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import './App.css';
 
 const App = () => (
   <Router>
@@ -14,7 +14,7 @@ const App = () => (
       <section className='container'>
         <Switch>
           <Route exact path='/register' component={Register} />
-          <Login exact path='/register' component={Login} />
+          <Login exact path='/login' component={Login} />
         </Switch>
       </section>
     </Fragment>

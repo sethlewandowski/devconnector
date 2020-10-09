0ddfb8bcbb1b82561478d131a263560237e08133
sethl <lewandowski.seth@gmail.com>
2020-10-09 07:52:42 -0700

    fixing errors

--- client/src/components/profile/ProfileExperience.js
+++ client/src/components/profile/ProfileExperience.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Moment from 'react-moment';
-import moment from 'moment';
 
 const ProfileExperience = ({
   experience: { company, title, location, current, to, from, description },

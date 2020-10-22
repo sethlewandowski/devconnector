5fd4075e41b9d2c6f9843b23bafdb17deb92baf0
sethl <lewandowski.seth@gmail.com>
2020-10-21 17:13:24 -0700

    running build on my machine

---
+++ client/build/precache-manifest.7d643d89ffb9150f2cf3dd58b79da2f5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58567a81f8b47484f8d5c6bbbfa18b4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a83a49c75dfffc49aca",
+    "url": "/static/css/main.7dd533de.chunk.css"
+  },
+  {
+    "revision": "bcd3c6cbed711224b1d8",
+    "url": "/static/js/2.c8fec910.chunk.js"
+  },
+  {
+    "revision": "320addaff3e37295a01ab28fa4b66477",
+    "url": "/static/js/2.c8fec910.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a83a49c75dfffc49aca",
+    "url": "/static/js/main.e9921832.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "aa9f56a0e938f4f6ba34122b4fdd9a63",
+    "url": "/static/media/showcase.aa9f56a0.jpg"
+  },
+  {
+    "revision": "e97252394df26fe87b09b12359a5a89c",
+    "url": "/static/media/spinner.e9725239.gif"
+  }
+]);

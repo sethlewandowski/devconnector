2e4b29597228b25572b12a91b53fc92a1c3c1e81
sethl <lewandowski.seth@gmail.com>
2020-10-22 11:00:24 -0700

    saving work

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
-const mongoose = require("mongoose");
-const config = require("config");
-const db = config.get("mongoURI");
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
@@ -11,7 +11,7 @@ const connectDB = async () => {
       useFindAndModify: false,
     });
 
-    console.log("MongoDB Connected...");
+    console.log('MongoDB Connected...');
   } catch (err) {
     console.log(err.message);
     //Exit Process with Failure
